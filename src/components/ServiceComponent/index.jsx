7b2035ef3fba0eb14b723fbb7c47ef6eb954354f
ariefhk <ariefrachmanhakim1001@gmail.com>
2023-05-14 21:54:20 +0700

    fix: service text space

--- src/components/ServiceComponent/index.jsx
+++ src/components/ServiceComponent/index.jsx
@@ -6,8 +6,8 @@ export default function ServiceComponent() {
   return (
     <Container className={styles.service}>
       <Row className="w-100 align-items-sm-center">
-        <Col sm={6} className={styles.serviceImage}>
-          <div className={styles.serviceImageBox}>
+        <Col lg={6} className={styles.service__image}>
+          <div className={styles.service__image__box}>
             <Image
               src="./images/service.svg"
               alt=""
@@ -16,38 +16,38 @@ export default function ServiceComponent() {
             />
           </div>
         </Col>
-        <Col sm={6} className={`d-flex align-items-end  mt-sm-0 mt-3`}>
-          <div className={styles.serviceText}>
+        <Col lg={6} className={`d-flex align-items-end  mt-sm-0 mt-3`}>
+          <div className={styles.service__text}>
             <h1>
-              Best Car Rental for any kind of trip in
-              <br className="d-sm-none d-block" /> Ciamis!
+              Best Car Rental for any kind <br className="d-sm-none d-block" />
+              of trip in <br className="d-none d-sm-block" /> Ciamis!
             </h1>
             <p>
-              Sewa mobil di Ciamis bersama Binar Car Rental jaminan harga lebih{" "}
-              <br className="d-sm-none d-block" />
+              Sewa mobil di Ciamis bersama Binar Car Rental jaminan harga lebih
+              <br className="d-none d-sm-block" />
               murah dibandingkan yang lain, kondisi mobil baru, serta kualitas
-              <br className="d-sm-none d-block" />
+              <br className="d-none d-sm-block" />
               pelayanan terbaik untuk perjalanan wisata, bisnis, wedding,
               meeting, dll.
             </p>
-            <ul className={styles.serviceText__link}>
-              <li className={styles.serviceText__check}>
+            <ul className={styles.service__text__link}>
+              <li className={styles.service__text__check}>
                 <Image src="./images/ic_blue_check.svg" alt="" srcSet="" />
                 <span>Sewa Mobil Dengan Supir di Bali 12 Jam</span>
               </li>
-              <li className={styles.serviceText__check}>
+              <li className={styles.service__text__check}>
                 <Image src="./images/ic_blue_check.svg" alt="" srcSet="" />
                 Sewa Mobil Lepas Kunci di Bali 24 Jam
               </li>
-              <li className={styles.serviceText__check}>
+              <li className={styles.service__text__check}>
                 <Image src="./images/ic_blue_check.svg" alt="" srcSet="" />
                 Sewa Mobil Jangka Panjang Bulanan
               </li>
-              <li className={styles.serviceText__check}>
+              <li className={styles.service__text__check}>
                 <Image src="./images/ic_blue_check.svg" alt="" srcSet="" />
                 Gratis Antar - Jemput Mobil di Bandara
               </li>
-              <li className={styles.serviceText__check}>
+              <li className={styles.service__text__check}>
                 <Image src="./images/ic_blue_check.svg" alt="" srcSet="" />
                 Layanan Airport Transfer / Drop In Out
               </li>
