df25829cf5ca26a0e127b332c5308c35a8873cab
ariefhk <ariefrachmanhakim1001@gmail.com>
2023-05-17 21:47:26 +0700

    fix: delete gitkeep

--- src/hooks/useInput.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-export default function useInput(defaultValue) {
-  const [input, setInput] = useState(defaultValue);
-  const handleInput = (event) => setInput(event.target.value);
-  const clearInput = () => setInput("");
-  return [input, handleInput, clearInput];
-}
